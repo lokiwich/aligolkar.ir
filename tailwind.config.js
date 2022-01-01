aacdd80a1c9bdb3bd0a61d5610d359bd0836fb2e
Ali Golkar <lokiwich@pm.me>
2022-01-02 00:41:20 +0330

    update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,6 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
-            colors: {
-                fuchsia: colors.fuchsia,
-                cyan: colors.cyan,
-                emerald: colors.emerald,
-                teal: colors.teal
-            },
             container: {
                 center: true,
             },
